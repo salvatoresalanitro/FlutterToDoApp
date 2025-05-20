739a3416f8742790da695d388120c26b05537c81
Salvo <salvatoresalanitro94@gmail.com>
2025-05-20 14:22:00 +0200

    change name main class and create pages/home_page folder and file

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/pages/home_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ToDoApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class ToDoApp extends StatelessWidget {
+  const ToDoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      home: HomePage(),
     );
   }
 }
