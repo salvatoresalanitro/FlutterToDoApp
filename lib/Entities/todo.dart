a66932e560eec91b5f5ed069236c2107bbf3b12a
Salvo <salvatoresalanitro94@gmail.com>
2025-05-28 09:05:28 +0200

    create new entity todo

---
+++ lib/Entities/todo.dart
@@ -0,0 +1,13 @@
+class Todo {
+  final String taskName;
+  final bool isChecked;
+
+  Todo({required this.taskName, required this.isChecked});
+
+  Todo copyWith(String? taskName, bool? isChecked ){
+    return Todo(
+      taskName: taskName?? this.taskName,
+      isChecked: isChecked ?? this.isChecked
+    );
+  }
+}
