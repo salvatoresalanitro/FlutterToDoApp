5c413f6afead182fede69b00b7f623397ad1a69e
Salvo <salvatoresalanitro94@gmail.com>
2025-05-28 15:50:16 +0200

    change todolist with workspaces

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -2,35 +2,30 @@ import 'package:hive_flutter/adapters.dart';
 import 'package:todo_app/Entities/todo.dart';
 
 class ToDoDatabase {
-  List<Todo> toDoList = [];
+  Map<String, List<Todo>> workspaces = {};
 
   //reference box
   final _toDoBox = Hive.box("ToDoBox");
 
   //it only runs the first time ever opening this app to put placeholder tasks
   void createInitialPlaceholderData() {
-    toDoList = [
-      Todo(taskName: "Watched the tutorial how to use the app", isChecked: true),
-      Todo(taskName: "Create your first todo", isChecked: false)
-    ];
+    workspaces = {
+      "Generale": [
+        Todo(taskName: "Watched the tutorial how to use the app", isChecked: true),
+        Todo(taskName: "Create your first todo", isChecked: false)
+      ],
+    };
   }
 
   //load the data from db
   void loadData() {
-  var rawList = _toDoBox.get("TODOLIST", defaultValue: []);
-  toDoList = (rawList as List).map((item) => Todo(
-    taskName: item["taskName"],
-    isChecked: item["isChecked"]
-  )).toList();
+  var rawData = _toDoBox.get("WORKSPACES", defaultValue: {});
+  workspaces = Map<String, List<Todo>>.from(rawData);
 }
 
 
   //update db
   void update() {
-    _toDoBox.put("TODOLIST", toDoList.map((todo) => {
-      "taskName": todo.taskName,
-      "isChecked": todo.isChecked
-      })
-    .toList());
+    _toDoBox.put("WORKSPACES", workspaces);
   }
 }
