c295d093dcb91ccd168efb4ddca48cd35c939111
Salvo <salvatoresalanitro94@gmail.com>
2025-06-03 15:55:09 +0200

    refactore firestore_service

--- lib/data/firestore_service.dart
+++ lib/data/firestore_service.dart
@@ -5,12 +5,14 @@ import 'package:todo_app/models/workspace.dart';
 class FirestoreService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
+  // -------------------- WORKSPACES --------------------
+
   Future<List<Workspace>> getUserWorkspaces(String userId) async {
     final workspaceSnapshots = await _firestore
-      .collection('users')
-      .doc(userId)
-      .collection('workspaces')
-      .get();
+        .collection('users')
+        .doc(userId)
+        .collection('workspaces')
+        .get();
 
     List<Workspace> workspaces = [];
 
@@ -36,24 +38,67 @@ class FirestoreService {
     return workspaces;
   }
 
-  Future<void> saveWorkspace(String userId, Workspace workspace) async {
-    final wsRef = _firestore.collection('users').doc(userId).collection('workspaces').doc(workspace.id);
-    await wsRef.set({'name': workspace.workspaceName});
+  Future<void> createOrUpdateWorkspace(String userId, Workspace workspace) async {
+    final wsRef = _firestore
+        .collection('users')
+        .doc(userId)
+        .collection('workspaces')
+        .doc(workspace.id);
 
-    for (var todo in workspace.todos) {
-      await wsRef.collection('todos').doc(todo.id).set({
-        'taskName': todo.taskName,
-        'isChecked': todo.isChecked,
-      });
-    }
+    await wsRef.set({'name': workspace.workspaceName});
+  }
+
+  Future<void> renameWorkspace(String userId, String workspaceId, String newName) async {
+    final wsRef = _firestore
+        .collection('users')
+        .doc(userId)
+        .collection('workspaces')
+        .doc(workspaceId);
+
+    await wsRef.update({'name': newName});
   }
 
   Future<void> deleteWorkspace(String userId, String workspaceId) async {
-    final wsRef = _firestore.collection('users').doc(userId).collection('workspaces').doc(workspaceId);
+    final wsRef = _firestore
+        .collection('users')
+        .doc(userId)
+        .collection('workspaces')
+        .doc(workspaceId);
+
     final todos = await wsRef.collection('todos').get();
     for (var doc in todos.docs) {
       await doc.reference.delete();
     }
+
     await wsRef.delete();
+  }
+
+  // -------------------- TODOS --------------------
+
+  Future<void> addOrUpdateTodo(String userId, String workspaceId, Todo todo) async {
+    final todoRef = _firestore
+        .collection('users')
+        .doc(userId)
+        .collection('workspaces')
+        .doc(workspaceId)
+        .collection('todos')
+        .doc(todo.id);
+
+    await todoRef.set({
+      'taskName': todo.taskName,
+      'isChecked': todo.isChecked,
+    });
+  }
+
+  Future<void> deleteTodo(String userId, String workspaceId, String todoId) async {
+    final todoRef = _firestore
+        .collection('users')
+        .doc(userId)
+        .collection('workspaces')
+        .doc(workspaceId)
+        .collection('todos')
+        .doc(todoId);
+
+    await todoRef.delete();
   }
 }
